2746ce571ddfe2cacd9d9827c9581e50fbe53c57
hunghg255 <giahung197bg@gmail.com>
2022-03-06 19:04:40 +0700

    update serviceworker

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,43 +3,49 @@ const social = [
     id: 'phone',
     name: 'Phone Number',
     value: '0374390536',
-    icon: './assets/images/call.svg'
+    icon: './assets/images/call.svg',
   },
   {
     id: 'email',
     name: 'Email',
     value: 'giahung197bg@gmail.com',
-    icon: './assets/images/email.svg'
+    icon: './assets/images/email.svg',
   },
   {
     id: 'fb',
     name: 'Facebook',
     value: 'https://facebook.com/hunghg255',
-    icon: './assets/images/facebook.svg'
+    icon: './assets/images/facebook.svg',
   },
   {
     id: 'instagram',
     name: 'Instagram',
     value: 'https://www.instagram.com/hello_silver_miu/',
-    icon: './assets/images/instagram.svg'
+    icon: './assets/images/instagram.svg',
   },
   {
     id: 'twitter',
     name: 'Twitter',
     value: 'https://twitter.com/hunghg255',
-    icon: './assets/images/twitter.svg'
+    icon: './assets/images/twitter.svg',
   },
   {
     id: 'linkedin',
     name: 'Linkedin',
     value: 'https://www.linkedin.com/in/hoanggiahung/',
-    icon: './assets/images/linkedin.svg'
+    icon: './assets/images/linkedin.svg',
   },
   {
     id: 'github',
     name: 'Github',
     value: 'https://github.com/hunghg255',
-    icon: './assets/images/github.svg'
+    icon: './assets/images/github.svg',
+  },
+  {
+    id: 'qr',
+    name: 'Qr code',
+    value: '',
+    icon: './assets/images/qr-code.png',
   },
 ];
 
@@ -51,10 +57,38 @@ document.addEventListener('DOMContentLoaded', () => {
     if (item.id === 'phone') link = `tel:${item.value}`;
     if (item.id === 'email') link = `mailto:${item.value}`;
 
-    const liStr = `<li><a href="${link}" target="_blank"><img src="${item.icon}" alt="" /><span>${item.name}</span></a></li>`;
+    let liStr = `<li><a href="${link}" target="_blank"><img src="${item.icon}" alt="" /><span>${item.name}</span></a></li>`;
+
+    if (item.id === 'qr')
+      liStr = `<li id="btnQr"><a href="javascript:void(0)"><img src="${item.icon}" alt="" /><span>${item.name}</span></a></li>`;
+
     acc += liStr;
     return acc;
   }, '');
-  console.log(list);
+
   socialListEle.innerHTML = list;
+
+  const btnQrCode = document.querySelector('#btnQr');
+  const qrCodeELe = document.querySelector('.qrCode');
+  const ignoreClickOnMeElement = document.querySelector('.imgQrCode');
+
+  btnQrCode.addEventListener('click', function () {
+    qrCodeELe.classList.toggle('active');
+  });
+
+  qrCodeELe.addEventListener('click', function (event) {
+    var isClickInsideElement = ignoreClickOnMeElement.contains(event.target);
+    if (!isClickInsideElement) {
+      qrCodeELe.classList.toggle('active');
+    }
+  });
 });
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker
+      .register('/serviceWorker.js')
+      .then((res) => console.log('service worker registered'))
+      .catch((err) => console.log('service worker not registered', err));
+  });
+}
---
+++ serviceWorker.js
@@ -0,0 +1,39 @@
+const staticDevCoffee = 'hung-xuan-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/assets/css/style.css',
+  '/assets/js/main.js',
+  '/assets/images/avatar.jpeg',
+  '/assets/images/call.svg',
+  '/assets/images/email.svg',
+  '/assets/images/facebook.svg',
+  '/assets/images/github.svg',
+  '/assets/images/instagram.svg',
+  '/assets/images/linkedin.svg',
+  '/assets/images/qr-code.png',
+  '/assets/images/qrhung.png',
+  '/assets/images/H16.png',
+  '/assets/images/H24.png',
+  '/assets/images/H32.png',
+  '/assets/images/H64.png',
+  '/assets/images/H128.png',
+  '/assets/images/H256.png',
+  '/assets/images/H512.png',
+];
+
+self.addEventListener('install', (installEvent) => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', (fetchEvent) => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then((res) => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
